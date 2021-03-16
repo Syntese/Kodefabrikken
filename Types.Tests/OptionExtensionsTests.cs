95e4593dd8e798176b438d70d0b1400abc369a27
pi3k14 <frode@sticos.no>
2021-03-16 21:01:40 +0100

    Created solution, Types project with Option type and a simple documentation.

---
+++ Types.Tests/OptionExtensionsTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Kodefabrikken.Types;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Types.Tests
+{
+    [TestClass]
+    public class OptionExtensionsTests
+    {
+        [TestMethod]
+        public void Value_type_transforms_to_nullable()
+        {
+            var value = 3;
+            var SUT = new Option<int>(value);
+
+            var result = SUT.ToNullable();
+
+            result.HasValue.Should().BeTrue();
+            result.Value.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void Empty_value_type_transforms_to_nullable()
+        {
+            var SUT = Option<int>.Empty;
+
+            var result = SUT.ToNullable();
+
+            result.HasValue.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Nullable_value_type_transforms_to_option()
+        {
+            var value = 3;
+            Nullable<int> v = value;
+
+            var result = v.ToOption();
+
+            result.HasValue.Should().BeTrue();
+            int resultValue = -1;
+            result.IfValue(p => resultValue = p);
+            resultValue.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void Empty_nullable_value_type_transforms_to_option()
+        {
+            Nullable<int> v = null;
+
+            var result = v.ToOption();
+
+            result.HasValue.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Value_type_transforms_to_option()
+        {
+            var value = 3;
+
+            var result = value.ToOption();
+
+            result.HasValue.Should().BeTrue();
+            var resultValue = -1;
+            result.IfValue(p => resultValue = p);
+            resultValue.Should().Be(value);
+        }
+
+        [TestMethod]
+        public void Reference_type_transforms_to_option()
+        {
+            OptionExtensionsTests value = new OptionExtensionsTests();
+
+            var result = value.ToOption();
+
+            result.HasValue.Should().BeTrue();
+            OptionExtensionsTests resultValue = null;
+            result.IfValue(p => resultValue = p);
+            resultValue.Should().BeSameAs(value);
+        }
+
+        [TestMethod]
+        public void Empty_reference_type_transforms_to_option()
+        {
+            OptionExtensionsTests value = null;
+
+            var result = value.ToOption();
+
+            result.HasValue.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Reference_type_transforms_to_object()
+        {
+            var value = new OptionExtensionsTests();
+            var SUT = new Option<OptionExtensionsTests>(value);
+
+            var result = SUT.ToObject();
+
+            result.Should().BeSameAs(value);
+        }
+
+        [TestMethod]
+        public void Emtpy_reference_type_transforms_to_object()
+        {
+            var SUT = Option<OptionExtensionsTests>.Empty;
+
+            var result = SUT.ToObject();
+
+            result.Should().BeNull();
+        }
+    }
+}
---
+++ Types/OptionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Kodefabrikken.Types
+{
+    public static class OptionExtensions
+    {
+        /// <summary>
+        /// Converts <see cref="Nullable{T}"/> to <see cref="Option{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of wrapped value, must be struct.</typeparam>
+        /// <param name="value">Nullable object to convert.</param>
+        /// <returns>Existing value wrapped in <see cref="Option{T}"/>.</returns>
+        public static Option<T> ToOption<T>(this Nullable<T> value) where T : struct
+            => value.HasValue ? new Option<T>(value.Value) : Option<T>.Empty;
+
+        /// <summary>
+        /// Converts <see cref="Option{T}"/> to <see cref="Nullable{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of wrapped value, must be struct.</typeparam>
+        /// <param name="option">Option object to convert.</param>
+        /// <returns>Existing value wrapped in <see cref="Nullable{T}"/>.</returns>
+        public static Nullable<T> ToNullable<T>(this Option<T> option) where T : struct
+        {
+            Nullable<T> retval = null;
+            option.IfValue(value => retval = value);
+
+            return retval;
+        }
+
+        /// <summary>
+        /// Converts object to <see cref="Option{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of wrapped value.</typeparam>
+        /// <param name="value">Value to wrap.</param>
+        /// <returns>Existing value wrapped in <see cref="Option{T}"/>.</returns>
+        public static Option<T> ToOption<T>(this T value)
+            => value != null ? new Option<T>(value) : Option<T>.Empty;
+
+        /// <summary>
+        /// Convert <see cref="Option{T}"/> to wrapped type.
+        /// </summary>
+        /// <typeparam name="T">Type of wrapped value, must be class.</typeparam>
+        /// <param name="option">Option object to convert.</param>
+        /// <returns>Existing value.</returns>
+        public static T ToObject<T>(this Option<T> option) where T : class
+        {
+            T result = null;
+            option.IfValue(value => result = value);
+
+            return result;
+        }
+    }
+}
